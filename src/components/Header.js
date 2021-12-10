c97b6a2d25be4ec1b9d5c450a67c2253f71feba2
Stefano Bisio <stefano.bisio@aia.co.uk>
2021-12-10 14:26:17 +0000

    Applying Routing rules to Add button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
+import { useLocation } from 'react-router-dom';
 
 const Header = ({title, onFormToggle, formShowingState}) => {
+
+    const location = useLocation()
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button 
-                color={formShowingState ? "red" : "green"}
-                copy={formShowingState ? "Close" : "Add"}
-                onClick={onFormToggle} />
+            {location.pathname === '/' && (
+                <Button 
+                    color={formShowingState ? "red" : "green"}
+                    copy={formShowingState ? "Close" : "Add"}
+                    onClick={onFormToggle} />
+                )
+            }
         </header>
     )
 }
